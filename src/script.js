b37cbad223ce7b7435148f6cb60315c022de2d94
Cristhyan Lima <cristhyanlima768@gmail.com>
2024-11-30 14:41:52 -0300

    corrections

--- src/script.js
+++ src/script.js
@@ -21,7 +21,7 @@ const navbar = document.getElementById("nav");
 const topLink = document.querySelector(".top-link");
 
 window.addEventListener("scroll", function () {
-  const scrollHeight = window.pageYOffset;
+  const scrollHeight = window.scrollY;
   const navHeight = navbar.getBoundingClientRect().height;
   if (scrollHeight > navHeight) {
     navbar.classList.add("fixed-nav");
